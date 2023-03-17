6542dbac1150f3eadc7a7a8c0f39a7fb7c694593
ModiSaumil <19bmiit001@gmail.com>
2023-03-17 15:54:32 +0530

    y

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
         <div className="form-container sign-up-container">
           <form action="#">
             <h1>Create Account</h1>
-            <div className="social-container">
+            {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-            </div>
-            <span>or use your email for registration</span>
+            </div> */}
+            <span>use your email for registration</span>
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
@@ -31,12 +31,12 @@ function App() {
         <div className="form-container sign-in-container">
           <form action="#">
             <h1>Sign in</h1>
-            <div className="social-container">
+            {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-            </div>
-            <span>or use your account</span>
+            </div> */}
+            <span>use your account</span>
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
